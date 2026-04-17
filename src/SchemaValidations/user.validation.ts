cd8d98a8b81f8d7987151feb5dace6bb2ed7064a
Saumz <bhowmicksaumyajeet@gmail.com>
2026-04-18 02:28:29 +0530

    Add user validation and model schemas

---
+++ src/SchemaValidations/user.validation.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+export const signUpValidation = z.object({
+    username: z.string()
+        .min(2, { message: "Username must have atleast 2 characters" })
+        .max(15, { message: "Username must not be above 15 characters" })
+        .regex(/^[a-zA-Z0-9_]+$/, { message: "Username must not have any special characters" }),
+    email: z.string().email({ message: "Email must be Valid Format" }),
+    password: z.string().min(6, { message: "Password should have min 6 characters" })
+})
+
+export const signInValidation = z.object({
+    email: z.string(),
+    password: z.string()
+})
+
+
+export const verifyCode = z.object({
+    code: z.string().length(6, { message: "Verification Code must be of exact 6 characters" })
+})
+
+export const acceptingMessageSchema = z.object({
+    acceptMessage: z.boolean()
+})
+
+export const messageSchemaValidation = z.object({
+    content: z.string()
+        .min(10, { message: "Content must have atleast 10 characters" })
+        .max(300, { message: "Content must not be above 300 characters" })
+})
+
---
+++ src/models/user.model.ts
@@ -0,0 +1,71 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+    content: string,
+    createdAt: Date
+}
+
+const messageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    }
+})
+
+
+export interface User extends Document {
+    username: string,
+    email: string,
+    password: string,
+    verifyCode: string,
+    verifyCodeExpiry: Date,
+    isVerified: boolean,
+    isAcceptingMessage: boolean,
+    message: Message[]  // this means the array type will be the Message type
+}
+
+
+const userSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Verification Code is required"],
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verification Code Expiry is requried"]
+    },
+    isVerified: {
+        type: Boolean,
+        required: [true, "Is Verified is needed"],
+        default: false
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        required: [true, "Is Accepting Message is needed"],
+        default: true
+    },
+    message: [messageSchema]
+})
+
+// here I am checking if the model is already created or we have to newly create it. Also have wrote the typescript for the following Code
+export const User = mongoose.models.User as mongoose.Model<User> || mongoose.model<User>("User", userSchema)
