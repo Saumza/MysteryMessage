db8f34dcd156c471d6d9d4aaca82e4d2204db06c
Saumza <saumyajeetbhowmick@gmail.com>
2026-05-01 21:15:51 +0530

    Add Some Packages

--- src/app/api/suggest-message/route.ts
+++ src/app/api/suggest-message/route.ts
@@ -4,7 +4,7 @@ import { google } from "@ai-sdk/google"
 export async function POST(req: Request) {
     try {
         const result = streamText({
-            model: google('gemini-2.0-flash'),
+            model: google('gemma-3-27b-it'),
             prompt: "Create a list of three open-ended andengaging questions formatted as a single string. Eachquestion should be separated by ' | | '. These questions arefor an anonymous social messaging platform, like Qooh.me,and should be suitable for a diverse audience. Avoidpersonal or sensitive topics, focusing instead onuniversal themes that encourage friendly interaction. Forexample, your output should be structured like this:'What's a hobby you've recently started? | | If you couldhave dinner with any historical figure, who would it be?|lWhat's a simple thing that makes you happy?'. Ensure thequestions are intriguing, foster curiosity, andcontribute to a positive and welcoming conversationalenvironment."
 
         })
