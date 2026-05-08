bdf0545c46a2299904917973de6fc23e6327eb75
Saumza <saumyajeetbhowmick@gmail.com>
2026-05-08 19:01:34 +0530

    Fix acceptMessage key in POST request to acceptMessages for consistency in schema validation

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -26,7 +26,7 @@ export async function POST(req: Request) {
         const { acceptMessage } = await req.json()
 
         const acceptMessageSchemaQuery = {
-            acceptMessage
+            acceptMessages: acceptMessage
         }
 
         const result = acceptingMessageSchema.safeParse(acceptMessageSchemaQuery)
