c7027cbe8c145b469ef598a3529fd9d011faa513
Saumza <saumyajeetbhowmick@gmail.com>
2026-05-08 13:56:03 +0530

    Update dependencies and enhance user dashboard with message suggestions feature

--- src/app/api/suggest-message/route.ts
+++ src/app/api/suggest-message/route.ts
@@ -2,9 +2,11 @@ import { convertToModelMessages, streamText, UIMessage } from "ai"
 import { google } from "@ai-sdk/google"
 
 export async function POST(req: Request) {
+    console.log("Route Hit");
+    
     try {
         const result = streamText({
-            model: google('gemini-2.5-flash'),
+            model: google('gemma-4-31b-it'),
             prompt: "Create a list of three open-ended andengaging questions formatted as a single string. Eachquestion should be separated by ' | | '. These questions arefor an anonymous social messaging platform, like Qooh.me,and should be suitable for a diverse audience. Avoidpersonal or sensitive topics, focusing instead onuniversal themes that encourage friendly interaction. Forexample, your output should be structured like this:'What's a hobby you've recently started? | | If you couldhave dinner with any historical figure, who would it be?|lWhat's a simple thing that makes you happy?'. Ensure thequestions are intriguing, foster curiosity, andcontribute to a positive and welcoming conversationalenvironment."
 
         })
--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -13,7 +13,9 @@ import { messageSchemaValidation } from '@/SchemaValidations/user.validation'
 import { toast } from 'sonner'
 import { APIResponse } from '@/types/APIResponse'
 import { Loader2 } from 'lucide-react'
+import { useCompletion } from '@ai-sdk/react'
 import { Separator } from '@/components/ui/separator'
+import Link from 'next/link'
 
 
 function page() {
@@ -48,11 +50,33 @@ function page() {
         }
     }
 
+    let initialMessages = "What's a simple pleasure that never fails to brighten your day? | | If you could instantly master any new skill, what would you choose? | | What's one fictional world you'd love to visit for a day?"
 
+    const { completion, complete, isLoading, error, setCompletion } = useCompletion({
+        api: '/api/suggest-message',
+        initialCompletion: initialMessages
+    });
+
+    const loadMessages = () => {
+        try {
+            complete("")
+        } catch (error) {
+            toast.error("Error While Loading Messages")
+        }
+    }
+
+    const parseResponse = (string: string) => {
+        const response = string.split(" | | ")
+        return response
+    }
+
+    useEffect(() => {
+        complete("")
+    }, [])
 
     return (
         <>
-            <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl font-outfit border border-black">
+            <div className="my-6 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl font-outfit ">
                 <h1 className="text-4xl font-bold mb-4 text-center">User Dashboard</h1>
                 <form onSubmit={form.handleSubmit(submitHandler)}>
                     <FieldGroup className='flex items-center'>
@@ -92,8 +116,50 @@ function page() {
                         </Button>
                     </FieldGroup>
                 </form>
+                <Separator className='mt-8' />
             </div>
-            <Separator />
+            <div className="mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl font-outfit ">
+                <Button type="submit" className='w-fit font-outfit p-5' disabled={isLoading} onClick={loadMessages}>
+                    {isLoading ? (
+                        <>
+                            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                            Please wait
+                        </>
+                    ) : (
+                        'Suggest Messages'
+                    )}
+                </Button>
+                <section className='font-outfit'>
+                    <p className='my-8 text-lg'>Click on any messages below to select it</p>
+                    <div className='border rounded-lg p-5'>
+                        <p className='font-semibold text-2xl'>Messages</p>
+                        <div className='flex flex-col my-4 gap-y-8'>
+                            {
+                                error ? (
+                                    <p className="text-red-500">{error.message}</p>
+                                ) : (
+                                    parseResponse(completion).map((message, index) => (
+                                        <Button
+                                            key={index}
+                                            onClick={() => form.setValue('content', message)}
+                                            className='bg-white text-black border border-gray-500 text-base p-8'
+                                        >
+                                            {message}
+                                        </Button>
+                                    ))
+                                )
+                            }
+                        </div>
+                    </div>
+                </section>
+                <Separator className='my-8' />
+                <div className='bg-black font-outfit text-white rounded-xl flex flex-col justify-center centre p-5 gap-y-5'>
+                    <p className='text-center text-2xl font-bold'>Get Your Anonymous Message</p>
+                    <Button className='w-fit bg-white text-black self-center text-base p-4'>
+                        <Link href={"/sign-up"}>Create Your Account</Link>
+                    </Button>
+                </div>
+            </div>
         </>
     )
 }
